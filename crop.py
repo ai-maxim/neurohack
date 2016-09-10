6aa4f0b1b7b443762693eca93f47ef83f235d124
zshimanchik <zahar.shimanchik@yandex.ru>
2016-09-10 21:45:21 +0300

    fixed someshit in crop

--- crop.py
+++ crop.py
@@ -6,18 +6,18 @@ from skimage.feature import canny
 
 
 
-img = io.imread('data/potato_src/1473498368.jpg', as_grey=True)  # this is a Numpy array with shape (3, 150, 150)
+img = io.imread('/home/neuron/projects/neurohack/data/potato_src/1473498588.jpg', as_grey=True)  # this is a Numpy array with shape (3, 150, 150)
 # Compute the Canny filter for two values of sigma
 edges = canny(img, sigma=3)
 
 # display results
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3), sharex=True, sharey=True)
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(8, 3), sharex=True, sharey=True)
 
 ax1.imshow(img, cmap=plt.cm.jet)
 ax1.axis('off')
 ax1.set_title('noisy image', fontsize=20)
 
-ax2.imshow(edges2, cmap=plt.cm.gray)
+ax2.imshow(edges, cmap=plt.cm.gray)
 ax2.axis('off')
 ax2.set_title('Canny filter, $\sigma=3$', fontsize=20)
 
