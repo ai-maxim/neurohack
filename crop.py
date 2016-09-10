76970eb702c00afa89f1c622fb559897aa1c379b
Xena Kirillova <xenakrll@gmail.com>
2016-09-10 21:40:27 +0300

    one canny

--- crop.py
+++ crop.py
@@ -4,35 +4,22 @@ from skimage import io
 import os
 from skimage.feature import canny
 
-'''
-# Generate noisy image of a square
-im = np.zeros((128, 128))
-im[32:-32, 32:-32] = 1
 
-im = ndi.rotate(im, 15, mode='constant')
-im = ndi.gaussian_filter(im, 4)
-im += 0.2 * np.random.random(im.shape)
-'''
 
 img = io.imread('data/potato_src/1473498368.jpg', as_grey=True)  # this is a Numpy array with shape (3, 150, 150)
 # Compute the Canny filter for two values of sigma
-edges1 = canny(img)
-edges2 = canny(img, sigma=3)
+edges = canny(img, sigma=3)
 
 # display results
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3), sharex=True, sharey=True)
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3), sharex=True, sharey=True)
 
 ax1.imshow(img, cmap=plt.cm.jet)
 ax1.axis('off')
 ax1.set_title('noisy image', fontsize=20)
 
-ax2.imshow(edges1, cmap=plt.cm.gray)
+ax2.imshow(edges2, cmap=plt.cm.gray)
 ax2.axis('off')
-ax2.set_title('Canny filter, $\sigma=1$', fontsize=20)
-
-ax3.imshow(edges2, cmap=plt.cm.gray)
-ax3.axis('off')
-ax3.set_title('Canny filter, $\sigma=3$', fontsize=20)
+ax2.set_title('Canny filter, $\sigma=3$', fontsize=20)
 
 fig.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9,
                     bottom=0.02, left=0.02, right=0.98)
