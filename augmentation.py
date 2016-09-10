90c957c719356fe5ef6fa9562e1493fa8596b537
zshimanchik <zahar.shimanchik@yandex.ru>
2016-09-10 17:29:49 +0300

    refactor autgumentation

--- augmentation.py
+++ augmentation.py
@@ -11,19 +11,25 @@ datagen = ImageDataGenerator(
         horizontal_flip=True,
         fill_mode='nearest')
 
-path = '/home/xenakrll/Pictures/kartoxa/test'
-imgs_paths = os.listdir(path)
-imgs_paths = [os.path.join(path, y) for y in imgs_paths]
-for img_path in imgs_paths:
-    img = io.imread(img_path, as_grey=True)  # this is a Numpy array with shape (3, 150, 150)
-    x = img_to_array(img)
-    x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
 
-# the .flow() command below generates batches of randomly transformed images
-# and saves the results to the `preview/` directory
-    i = 0
-    for batch in datagen.flow(x, batch_size=1,
-                              save_to_dir='/home/xenakrll/Pictures/kartoxa/res/', save_prefix='potato', save_format='jpeg'):
-        i += 1
-        if i > 20:
-            break  # otherwise the generator would loop indefinitely
+def augument_dir(src_path, dist_path):
+    imgs_paths = os.listdir(src_path)
+    imgs_paths = [os.path.join(src_path, y) for y in imgs_paths]
+    for img_path in imgs_paths:
+        img = io.imread(img_path, as_grey=True)  # this is a Numpy array with shape (3, 150, 150)
+        x = img_to_array(img)
+        x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+
+        # the .flow() command below generates batches of randomly transformed images
+        # and saves the results to the `preview/` directory
+        i = 0
+        for batch in datagen.flow(x, batch_size=1,
+                                  save_to_dir=dist_path, save_prefix='potato',
+                                  save_format='jpeg'):
+            i += 1
+            if i > 20:
+                break  # otherwise the generator would loop indefinitely
+
+augument_dir('data/potato_src', 'data/potato')
+augument_dir('data/not_potato_src', 'data/not_potato')
+
