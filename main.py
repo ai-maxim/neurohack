967c6977c499c6bdaefde9a8d5d2e5a02af5a1a1
zshimanchik <zahar.shimanchik@yandex.ru>
2016-09-10 23:54:30 +0300

    model and classifier

---
+++ main.py
@@ -0,0 +1,38 @@
+from keras.preprocessing.image import ImageDataGenerator
+from classifier import create_model
+model = create_model()
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is a generator that will read pictures found in
+# subfolers of 'data/train', and indefinitely generate
+# batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        'data/train',  # this is the target directory
+        target_size=(150, 150),  # all images will be resized to 150x150
+        batch_size=32,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        'data/validation',
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    samples_per_epoch=400,
+    nb_epoch=50,
+    validation_data=validation_generator,
+    nb_val_samples=400)
+model.save_weights('first_try.h5')  # always save your weights after training or during training
